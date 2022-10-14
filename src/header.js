4fa054f4ac4f09571b93d2f4fca41d3c9a86bbe4
Rikin <rikinbhalodia9504@gmail.com>
2022-10-14 23:01:13 +0530

    header part

---
+++ src/header.js
@@ -0,0 +1,72 @@
+import React from "react";
+import styled from "styled-components";
+import Mic from "./assets/images/studio/header/mic.svg";
+import Search from "./assets/images/studio/header/search.svg";
+import MainLogo from "./assets/images/studio/header/main-logo.svg";
+import Heart from "./assets/images/studio/header/heart.svg";
+import Cart from "./assets/images/studio/header/cart.svg";
+import Profile from "./assets/images/studio/header/profile.svg";
+
+const HeaderWrapper = styled.div`
+  background: rgb(102 94 47);
+  height: 100px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  .search {
+    background: rgba(255, 255, 255, 0.6);
+    border: 1px solid rgba(0, 0, 0, 0.09);
+    border-radius: 136px;
+    height: 30px;
+    width: 250px;
+    padding: 0 20px;
+  }
+  .search-icon {
+    position: relative;
+    right: 25px;
+    height: 15px;
+    top: 2px;
+    opacity: 0.5;
+  }
+  .mic {
+    height: 20px;
+    width: 20px;
+  }
+  .left-part {
+  }
+  .center-part {
+  }
+  .right-part {
+    width: 200px;
+    display: flex;
+    justify-content: space-between;
+  }
+  .right-image {
+    height: 20px;
+    width: 20px;
+  }
+`;
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <div className="left-part">
+        <input placeholder="Search Items" className="search" />
+        <img src={Search} alt="search" className="search-icon" />
+
+        <img src={Mic} alt="mic" className="mic" />
+      </div>
+      <div className="center-part">
+        <img src={MainLogo} alt="search" />
+      </div>
+      <div className="right-part">
+        <img src={Profile} alt="mic" className="right-image" />
+        <img src={Heart} alt="search" className="right-image" />
+
+        <img src={Cart} alt="search" className="right-image" />
+      </div>
+    </HeaderWrapper>
+  );
+};
+
+export default Header;
