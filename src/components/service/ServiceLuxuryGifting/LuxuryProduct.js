aac26305df1d04a3963f0ed963a85e5f8df93012
Rikin <rikinbhalodia9504@gmail.com>
2022-11-03 11:48:36 +0530

    add remaining part of the service product for him

--- src/components/service/ServiceLuxuryGifting/LuxuryProduct.js
+++ src/components/service/ServiceLuxuryGifting/LuxuryProduct.js
@@ -23,6 +23,34 @@ const LuxuryProductWrapper = styled.div`
       margin-bottom: 30px;
     }
   }
+  .gift {
+    font-family: "Mulish";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    text-transform: uppercase;
+    text-align: center;
+    padding-top: 40px;
+  }
+  .card {
+    background: #ffe600;
+    height: 150px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 10px;
+    text-transform: uppercase;
+    font-style: normal;
+    font-weight: 400;
+  }
+  .buy-gift {
+    font-family: "Coral Blush";
+    font-size: 40px;
+  }
+  .desc {
+    font-family: "Mulish";
+    font-size: 17px;
+  }
 `;
 
 const LuxuryProduct = () => {
@@ -35,6 +63,13 @@ const LuxuryProduct = () => {
           <ProductImgSlider />
         </div>
         <YouMayLike />
+        <div>
+          <div className="gift">Not sure what to gift? </div>
+          <div className="card">
+            <div className="buy-gift">buy a gift card</div>
+            <div className="desc">and give them freedom to choose</div>
+          </div>
+        </div>
       </Suspense>
     </LuxuryProductWrapper>
   );
