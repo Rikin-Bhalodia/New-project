100dcba70d807b6c8cdc0aab45aa1e1883efd6d3
Rikin <rikinbhalodia9504@gmail.com>
2022-11-07 01:34:33 +0530

    fix design wanna shop

---
+++ src/commonComponents/WannaShop/SearchFilters.js
@@ -0,0 +1,157 @@
+import { Slider } from "antd";
+import React from "react";
+import styled from "styled-components";
+
+const SearchFiltersWrapper = styled.div`
+  .input {
+    width: 210px !important;
+    z-index: 9999;
+    margin-left: 15px;
+    color: white;
+    ::placeholder {
+      color: white;
+      opacity: 1;
+    }
+  }
+  .search-input-brand {
+    width: 340px;
+    background: transparent;
+    padding-bottom: 10px;
+    border-bottom: 1px solid white !important;
+    font-style: italic;
+    border: none;
+    color: #a75b41;
+    z-index: 99;
+    font-size: 33px !important;
+  }
+  .contents {
+    margin-top: 20px;
+    font-family: "Arial";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    width: 100%;
+  }
+  .maintain-width {
+    width: 210px;
+  }
+  .color-input {
+    width: 345px;
+    font-size: 20px;
+    padding: 0 20px;
+    margin-top: 25px;
+  }
+  .color-collection {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 15px;
+    margin-top: 20px;
+  }
+  .range {
+    display: flex;
+    flex-direction: column;
+    font-family: "Arial";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 15px;
+    letter-spacing: -0.408px;
+    margin-top: 40px;
+  }
+  .range-shower {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 15px;
+  }
+  .close {
+    position: relative;
+    top: 18px;
+    font-weight: 800;
+    font-size: 18px;
+    cursor: pointer;
+  }
+  .range-box {
+    position: absolute;
+    background: #ffffff;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    height: 300px;
+    width: 380px;
+    padding: 15px;
+    z-index: 999;
+    display: flex;
+    flex-direction: column;
+    color: black;
+  }
+`;
+
+const SearchFilters = ({ background, setBackground, type, data }) => {
+  return (
+    <SearchFiltersWrapper>
+      <div
+        className={` ${background === type ? "search-part" : "maintain-width"}`}
+      >
+        <div className={background === type && "search-box"}>
+          {background === type && <div className="head">search</div>}
+          <div className="search-logo">
+            <input
+              placeholder={type}
+              alt="search"
+              type="text"
+              className={`search-input-brand ${
+                !(background === type) && "input"
+              }`}
+              onClick={() => setBackground(type)}
+            />
+            {background === type && (
+              <div className="close" onClick={() => setBackground("")}>
+                X
+              </div>
+            )}
+          </div>
+          {background === type && type !== "Color" && type !== "Price Range" && (
+            <div className="contents">
+              {data.map((data) => (
+                <div className="content"> {data}</div>
+              ))}
+            </div>
+          )}
+          {background === type && type === "Color" && (
+            <div>
+              <input
+                placeholder="Enter HEX Code            #000000"
+                type="text"
+                className="color-input"
+              />
+              <div className="color-collection">
+                {[1, 2, 3, 3, 4, , 5, 6, 7, 7, 8, 8, 9, 9, , 0, 0].map((_) => {
+                  return (
+                    <div
+                      style={{
+                        background: "black",
+                        height: "20px",
+                        width: "20px",
+                      }}
+                    ></div>
+                  );
+                })}
+              </div>
+            </div>
+          )}
+          {background === type && type === "Price Range" && (
+            <div className="range">
+              <div>SET RANGE</div>
+              <div className="range-shower">
+                <div>Rs. 100</div>
+                <div>Rs. 10,00,000</div>
+              </div>
+              <div>
+                <Slider defaultValue={30} min={100} max={10000} />
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </SearchFiltersWrapper>
+  );
+};
+
+export default SearchFilters;
--- src/commonComponents/WannaShop/index.js
+++ src/commonComponents/WannaShop/index.js
@@ -1,8 +1,8 @@
-import { Slider } from "antd";
 import React, { useState } from "react";
 import styled from "styled-components";
 import LeftFilter from "../../assets/images/product/left-filter.svg";
 import RightFilter from "../../assets/images/product/right-filter.svg";
+import SearchFilters from "./SearchFilters";
 
 const WannaShopWrapper = styled.div`
   .middle-part {
@@ -11,6 +11,11 @@ const WannaShopWrapper = styled.div`
     gap: 30px;
     align-items: center;
     justify-content: center;
+  }
+
+  .search-part {
+    position: relative;
+    top: -80px;
   }
   .search-input {
     border: none;
@@ -24,16 +29,28 @@ const WannaShopWrapper = styled.div`
     border-bottom: 1px solid #a75b41 !important;
     background: transparent;
   }
-  .search-input-brand {
-    width: 340px;
-    background: transparent;
-    padding-bottom: 10px;
-    border-bottom: 1px solid white !important;
-    font-style: italic;
-    border: none;
-    color: #a75b41;
-    font-size: 33px !important;
+  .search-box {
+    position: absolute;
+    background: #ffffff;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    height: 400px;
+    width: 380px;
+    padding: 15px;
+    z-index: 999;
+    display: flex;
+    flex-direction: column;
+    color: black;
   }
+  .head {
+    text-transform: uppercase;
+    align-self: flex-start;
+    font-size: 16px;
+  }
+  .search-logo {
+    display: flex;
+    margin-right: 20px;
+  }
+
   .des {
     text-align: center;
     font-family: "Arial";
@@ -88,110 +105,16 @@ const WannaShopWrapper = styled.div`
     margin-bottom: 15px;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   }
-  .contents {
-    margin-top: 20px;
-    font-family: "Arial";
-    font-style: normal;
-    font-weight: 400;
-    font-size: 18px;
-    width: 100%;
-  }
-  .head {
-    text-transform: uppercase;
-    align-self: flex-start;
-    font-size: 16px;
-  }
-  .search-part {
-    position: relative;
-    top: -80px;
-  }
-  .search-box {
-    position: absolute;
-    background: #ffffff;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    height: 400px;
-    width: 380px;
-    padding: 15px;
-    z-index: 999;
-    display: flex;
-    flex-direction: column;
-    color: black;
-  }
-  .range-box {
-    position: absolute;
-    background: #ffffff;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    height: 300px;
-    width: 380px;
-    padding: 15px;
-    z-index: 999;
-    display: flex;
-    flex-direction: column;
-    color: black;
-  }
+
   input:focus {
     outline-offset: 0px;
     outline: none;
   }
-  .close {
-    position: relative;
-    right: 15px;
-    top: 35px;
-    color: white;
-    font-weight: 800;
-    font-size: 18px;
-    cursor: pointer;
-  }
-  .close-color {
-    color: "white";
-  }
-  .search-logo {
-    display: flex;
-    margin-right: 20px;
-  }
-  .input {
-    width: 210px !important;
-    z-index: 9999;
-    margin-left: 15px;
-    color: white;
-    ::placeholder {
-      color: white;
-      opacity: 1;
-    }
-  }
-  .maintain-width {
-    width: 210px;
-  }
+
   .filter-part {
     display: flex;
   }
-  .range {
-    display: flex;
-    flex-direction: column;
-    font-family: "Arial";
-    font-style: normal;
-    font-weight: 700;
-    font-size: 15px;
-    letter-spacing: -0.408px;
-    margin-top: 40px;
-  }
-  .range-shower {
-    display: flex;
-    justify-content: space-between;
-    margin-top: 15px;
-  }
-  .color-input {
-    width: 345px;
-    font-size: 20px;
-    padding: 0 20px;
-    margin-top: 25px;
-  }
-  .color-collection {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 15px;
-    margin-top: 20px;
-  }
+
   .sort-by {
     background: #a75b41;
     border: 1px solid #a75b41;
@@ -307,30 +230,33 @@ const data = ["halter dress", "strapless dress", "versace dress"];
 
 const WannaShop = () => {
   const [background, setBackground] = useState(false);
-  const [backgroundBrand, setBackgroundBrand] = useState(false);
-  const [backgroundColor, setBackgroundColor] = useState(false);
-  const [backgroundPriceRange, setBackgroundPriceRange] = useState(false);
+  const [searchBackground, setSearchBackground] = useState(false);
 
   return (
     <WannaShopWrapper>
       <div className="middle-part">
         <div className="heading">I WANNA SHOP</div>
-        <div className={background && "search-part"}>
-          <div className={background && "search-box"}>
-            {background && <div className="head">search</div>}
+        <div className={searchBackground && "search-part"}>
+          <div className={searchBackground && "search-box"}>
+            {searchBackground && <div className="head">search</div>}
             <div className="search-logo">
               <input
                 placeholder="search here"
                 alt="search"
                 type="text"
                 className="search-input"
-                onClick={() => setBackground(true)}
+                onClick={() => setSearchBackground(true)}
               />
-              <div className="close" onClick={() => setBackground(false)}>
-                X
-              </div>
+              {searchBackground && (
+                <div
+                  className="close"
+                  onClick={() => setSearchBackground(false)}
+                >
+                  X
+                </div>
+              )}
             </div>
-            {background && (
+            {searchBackground && (
               <div className="contents">
                 {data.map((data) => (
                   <div className="content"> {data}</div>
@@ -345,159 +271,41 @@ const WannaShop = () => {
         <img src={LeftFilter} alt="left" className="image" />
         <div className="filter-part">
           <span>I’m looking for</span>
-          <div
-            className={` ${backgroundBrand ? "search-part" : "maintain-width"}`}
-          >
-            <div className={backgroundBrand && "search-box"}>
-              {backgroundBrand && <div className="head">search</div>}
-              <div className="search-logo">
-                <input
-                  placeholder="Brand"
-                  alt="search"
-                  type="text"
-                  className={`search-input-brand ${
-                    !backgroundBrand && "input"
-                  }`}
-                  onClick={() => setBackgroundBrand(true)}
-                />
-                <div
-                  className="close"
-                  onClick={() => setBackgroundBrand(false)}
-                >
-                  X
-                </div>
-              </div>
-              {backgroundBrand && (
-                <div className="contents">
-                  {data.map((data) => (
-                    <div className="content"> {data}</div>
-                  ))}
-                </div>
-              )}
-            </div>
-          </div>
+          <SearchFilters
+            type="Brand"
+            setBackground={setBackground}
+            background={background}
+            css="search-box"
+            data={data}
+          />
+
           <span style={{ margin: "0 0 0 20px" }}>with</span>
-          <div
-            className={` ${backgroundBrand ? "search-part" : "maintain-width"}`}
-          >
-            <div className={backgroundBrand && "search-box"}>
-              {backgroundBrand && <div className="head">search</div>}
-              <div className="search-logo">
-                <input
-                  placeholder="Him/Her"
-                  alt="search"
-                  type="text"
-                  className={`search-input-brand ${
-                    !backgroundBrand && "input"
-                  }`}
-                  onClick={() => setBackgroundBrand(true)}
-                />
-                <div
-                  className="close"
-                  onClick={() => setBackgroundBrand(false)}
-                >
-                  X
-                </div>
-              </div>
-              {backgroundBrand && (
-                <div className="contents">
-                  {data.map((data) => (
-                    <div className="content"> {data}</div>
-                  ))}
-                </div>
-              )}
-            </div>
-          </div>
+          <SearchFilters
+            type="Him/Her"
+            setBackground={setBackground}
+            background={background}
+            css="search-box"
+            data={data}
+          />
+
           <span style={{ padding: "0 0 0 20px" }}>of</span>
-          <div
-            className={` ${backgroundColor ? "search-part" : "maintain-width"}`}
-          >
-            <div className={backgroundColor && "search-box"}>
-              {backgroundColor && <div className="head">search</div>}
-              <div className="search-logo">
-                <input
-                  placeholder="Color"
-                  alt="search"
-                  type="text"
-                  className={`search-input-brand ${
-                    !backgroundColor && "input"
-                  }`}
-                  onClick={() => setBackgroundColor(true)}
-                />
-                <div
-                  className="close"
-                  onClick={() => setBackgroundColor(false)}
-                >
-                  X
-                </div>
-              </div>
-              {backgroundColor && (
-                <div>
-                  <input
-                    placeholder="Enter HEX Code            #000000"
-                    type="text"
-                    className="color-input"
-                  />
-                  <div className="color-collection">
-                    {[1, 2, 3, 3, 4, , 5, 6, 7, 7, 8, 8, 9, 9, , 0, 0].map(
-                      (_) => {
-                        return (
-                          <div
-                            style={{
-                              background: "black",
-                              height: "20px",
-                              width: "20px",
-                            }}
-                          ></div>
-                        );
-                      }
-                    )}
-                  </div>
-                </div>
-              )}
-            </div>
-          </div>
+          <SearchFilters
+            type="Color"
+            setBackground={setBackground}
+            background={background}
+            css="search-box"
+            data={data}
+          />
         </div>
         <div style={{ display: "flex" }}>
           <span>with a</span>
-          <div
-            className={` ${
-              backgroundPriceRange ? "search-part" : "maintain-width"
-            }`}
-          >
-            <div className={backgroundPriceRange && "range-box"}>
-              {backgroundPriceRange && <div className="head">set</div>}
-              <div className="search-logo">
-                <input
-                  placeholder="Price Range"
-                  alt="search"
-                  type="text"
-                  className={`search-input-brand ${
-                    !backgroundPriceRange && "input"
-                  }`}
-                  onClick={() => setBackgroundPriceRange(true)}
-                />
-                <div
-                  className="close"
-                  onClick={() => setBackgroundPriceRange(false)}
-                >
-                  X
-                </div>
-              </div>
-              {backgroundPriceRange && (
-                <div className="range">
-                  <div>SET RANGE</div>
-                  <div className="range-shower">
-                    <div>Rs. 100</div>
-                    <div>Rs. 10,00,000</div>
-                  </div>
-                  <div>
-                    <Slider defaultValue={30} min={100} max={10000} />
-                  </div>
-                </div>
-              )}
-            </div>
-          </div>
+          <SearchFilters
+            type="Price Range"
+            setBackground={setBackground}
+            background={background}
+            css="range-box"
+            data={data}
+          />
         </div>
         <img src={RightFilter} alt="right" className="image2" />
       </div>
