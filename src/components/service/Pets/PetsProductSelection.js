ec18e6458f3d38b1b8c7803f8c45c1049de4e7f1
Rikin <rikinbhalodia9504@gmail.com>
2022-11-19 18:30:30 +0530

    pets product selection page responsive compelted

--- src/components/service/Pets/PetsProductSelection.js
+++ src/components/service/Pets/PetsProductSelection.js
@@ -105,6 +105,59 @@ const PetsProductSelectionWrapper = styled.div`
     font-size: 16px;
     margin-left: 10px;
   }
+  .image-section {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+  }
+  @media (max-width: 700px) {
+    .text-img {
+      width: 50%;
+      margin-top: 30px;
+      margin: 0 0 30px 30px;
+    }
+    .products {
+      display: flex;
+      flex-direction: column;
+      row-gap: 30px;
+    }
+    .bought-together {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+    .checkbox-fruit {
+      display: flex;
+      align-items: center;
+    }
+    .label {
+      flex: 1;
+    }
+    .input-checkbox {
+      font-size: 16px;
+    }
+    .discount {
+      font-size: 14px;
+    }
+    .btn {
+      width: 150px;
+      font-size: 16px;
+      height: 44px;
+    }
+  }
+  @media (max-width: 500px) {
+    .input-checkbox {
+      font-size: 14px;
+    }
+    .discount {
+      font-size: 12px;
+    }
+    .plus-icon {
+      font-size: 65px;
+    }
+  }
 `;
 
 const PetsProductSelection = () => {
@@ -112,70 +165,77 @@ const PetsProductSelection = () => {
     <PetsProductSelectionWrapper>
       <Suspense fallback="Loading...">
         <ProductSelectionCommon />
-        <img src={Text} alt="text" loading="lazy" className="text-img" />
-        <div className="products">
-          <img
-            src={Image1}
-            alt="cart-product"
-            loading="lazy"
-            className="images"
-          />
-          <div className="plus-icon">+</div>
-          <img
-            src={Image2}
-            alt="cart-product"
-            loading="lazy"
-            className="images"
-          />
-          <div className="prices">
-            <div className="total">
-              <div className="total-price">TOTAL PRICE:</div>
-              <div className="mrp">₹1,725.00</div>
-              <div className="discount-mrp">₹1,638.75</div>
+        <div className="bought-together">
+          <img src={Text} alt="text" loading="lazy" className="text-img" />
+          <div className="products">
+            <div className="image-section">
+              <img
+                src={Image1}
+                alt="cart-product"
+                loading="lazy"
+                className="images"
+              />
+              <div className="plus-icon">+</div>
+              <img
+                src={Image2}
+                alt="cart-product"
+                loading="lazy"
+                className="images"
+              />
             </div>
-            <div className="btn">ADD TO CART</div>
+            <div className="prices">
+              <div className="total">
+                <div className="total-price">TOTAL PRICE:</div>
+                <div className="mrp">₹1,725.00</div>
+                <div className="discount-mrp">₹1,638.75</div>
+              </div>
+              <div className="btn">ADD TO CART</div>
+            </div>
+          </div>
+          <div className="input-checkbox">
+            <div className="checkbox-fruit">
+              <input
+                type="checkbox"
+                id="fruit"
+                name="fruit"
+                value="fruit"
+                className="checkbox"
+              />
+              <label
+                for="fruit"
+                style={{
+                  cursor: "pointer",
+                }}
+                className="label"
+              >
+                This item: Versele Laga Food for Parrots - Exotic Fruit{" "}
+                <span className="mrp">₹990.00</span>
+                <span className="discount">₹940.50</span>
+              </label>
+            </div>
+            <div className="checkbox-fruit">
+              <input
+                type="checkbox"
+                id="food"
+                name="food"
+                value="food"
+                className="checkbox"
+              />
+              <label
+                for="food"
+                style={{
+                  cursor: "pointer",
+                }}
+                className="label"
+              >
+                Versele Laga Food for Parrots - Prestige
+                <span className="mrp">₹735.00</span>
+                <span className="discount">₹698.25</span>
+              </label>
+            </div>
           </div>
         </div>
-        <div className="input-checkbox">
-          <div>
-            <input
-              type="checkbox"
-              id="fruit"
-              name="fruit"
-              value="fruit"
-              className="checkbox"
-            />
-            <label
-              for="fruit"
-              style={{
-                cursor: "pointer",
-              }}
-            >
-              This item: Versele Laga Food for Parrots - Exotic Fruit{" "}
-              <span className="mrp">₹990.00</span>
-              <span className="discount">₹940.50</span>
-            </label>
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              id="food"
-              name="food"
-              value="food"
-              className="checkbox"
-            />
-            <label
-              for="food"
-              style={{
-                cursor: "pointer",
-              }}
-            >
-              Versele Laga Food for Parrots - Prestige
-              <span className="mrp">₹735.00</span>
-              <span className="discount">₹698.25</span>
-            </label>
-          </div>
-        </div>
+
         <AboutProduct />
         <div className="review">REVIEWS</div>
         <ReviewSlider
